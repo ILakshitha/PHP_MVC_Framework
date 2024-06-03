ca5d651dfe82e8537010cc6b40734232a57c0919
ILakshitha <ilakshitha7921@gmail.com>
2024-06-03 23:56:16 +0530

    create View Modularization

---
+++ app/views/inc/components/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo SITENAME;?></title>
+                         
+    <meta name='viewport' content="width-device-width, initial-scaler-1.0">
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/css/style.css">
+</head>
+<body>
--- app/views/v_about.php
+++ app/views/v_about.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    
-    <title><?php echo SITENAME;?></title>
-</head>
-<body>
-    <h1>this is the about view. Hi <?php echo $data['userName']; ?></h1>
-    <h2>And i am<?php echo $data['userAge']; ?></h2>
-
-    <h1> APPROOT <?php echo APPROOT; ?></h1>
-</body>
-</html>
+<?php require APPROOT."/views/inc/components/header.php"; ?>
+<h1> PHP_MVC_Framework</h1>
+<?php require APPROOT."/views/inc/components/footer.php"; ?>
